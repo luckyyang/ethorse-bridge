91f727dbe7ce267072de553286dac5207721abe1
ethorse <snipertech@protonmail.com>
2018-01-28 13:53:08 -0700

    updating infura endpoint

--- js/Contract/ContractController.js
+++ js/Contract/ContractController.js
@@ -9,7 +9,7 @@ var Web3 = require('web3');
 var contract = require("truffle-contract");
 var controllerjson=require('../..//json/BettingController.json');
 var samplejson=require('../../json/ETHorse.json');
-const provider = `wss://ropsten.infura.io/ws`
+const provider = `wss://ropsten.infura.io/_ws`
 // let web3 = new Web3(new Web3.providers.WebsocketProvider("wss://ropsten.infura.io/ws"))
 let web3 = new Web3(new Web3.providers.WebsocketProvider("ws://192.168.0.112:8546"))
 // var Contract = require('./Contract/Contract');
@@ -21,21 +21,21 @@ var options={address:contractAddress,topics:['0xbb72155379e773b51e75e9105a92b43c
 var sub = web3.eth.subscribe('logs',options,function(logs){
 });
 sub.on('data',function(result){
-    
+
 let contractdetails=(web3.eth.abi.decodeLog([
             {
                 "indexed": false,
                 "name": "_address",
                 "type": "address"
             },
-            
+
             {
                 "indexed": false,
                 "name": "_owner",
                 "type": "address"
             },
-            
-            
+
+
             {
                 "indexed": false,
                 "name": "_time",
@@ -68,7 +68,7 @@ router.post('/', function (req, res) {
     Contract.create({
             contractid : req.body.contractid,
             date : req.body.date
-        }, 
+        },
         function (err, contract) {
             console.log(contract);
             if (err) return res.status(500).send("There was a problem adding the information to the database.");
