57c384a6ee7507fed92d33089aec010ec9b4834e
starbuoy <starbuoy@protonmail.com>
2018-01-28 09:46:29 -0700

    Listener sync with DB

--- js/Contract/ContractController.js
+++ js/Contract/ContractController.js
@@ -4,7 +4,64 @@ var bodyParser = require('body-parser');
 router.use(bodyParser.urlencoded({ extended: true }));
 router.use(bodyParser.json());
 
+
+var Web3 = require('web3');
+var contract = require("truffle-contract");
+var controllerjson=require('../..//json/BettingController.json');
+var samplejson=require('../../json/ETHorse.json');
+const provider = `wss://ropsten.infura.io/ws`
+// let web3 = new Web3(new Web3.providers.WebsocketProvider("wss://ropsten.infura.io/ws"))
+let web3 = new Web3(new Web3.providers.WebsocketProvider("ws://192.168.0.112:8546"))
+// var Contract = require('./Contract/Contract');
+var contractAddress="0xacadf60516a37402bedf03e37cd3fccef9078524";
+
+let contractInstance = new web3.eth.Contract(controllerjson, contractAddress);
+var options={address:contractAddress,topics:['0xbb72155379e773b51e75e9105a92b43c2b89e37a51b13c2780ae34929b89457d']};
+
+var sub = web3.eth.subscribe('logs',options,function(logs){
+});
+sub.on('data',function(result){
+    
+let contractdetails=(web3.eth.abi.decodeLog([
+            {
+                "indexed": false,
+                "name": "_address",
+                "type": "address"
+            },
+            
+            {
+                "indexed": false,
+                "name": "_owner",
+                "type": "address"
+            },
+            
+            
+            {
+                "indexed": false,
+                "name": "_time",
+                "type": "uint256"
+            }
+        ],result.data,result.topics));
+storeContract(contractdetails);
+});
+
+
+
+
+
+
 var Contract = require('./Contract');
+
+var storeContract= function(contractdetails)
+    {
+
+    console.log(contractdetails)
+    Contract.create({
+            contractid : contractdetails._address,
+            date : contractdetails._time
+    });
+    }
+
 
 router.post('/', function (req, res) {
     console.log(req.body)
@@ -13,10 +70,10 @@ router.post('/', function (req, res) {
             date : req.body.date
         }, 
         function (err, contract) {
+            console.log(contract);
             if (err) return res.status(500).send("There was a problem adding the information to the database.");
             res.status(200).send(contract);
-        });
-
+    });
 });
 
 router.get('/', function (req, res) {
@@ -30,3 +87,4 @@ router.get('/', function (req, res) {
 
 
 module.exports = router;
+// module.exports = storeContract;
--- js/app.js
+++ js/app.js
@@ -1,101 +1,12 @@
 // app.js
 var express = require('express');
 var app = express();
-// var db = require('./db');
-var Web3 = require('web3');
-var contract = require("truffle-contract");
-var controllerjson=require('../json/BettingController.json');
-var samplejson=require('../json/ETHorse.json');
-// console.log(new Web3.providers.HttpProvider("https://ropsten.infura.io/y4sgTWmRT6xvgCrjHmN2").isConnected())
-const provider = `wss://ropsten.infura.io/ws`
-let web3 = new Web3(new Web3.providers.WebsocketProvider("wss://ropsten.infura.io/ws"))
-
-var contractAddress="0xacadf60516a37402bedf03e37cd3fccef9078524";
-
-let contractInstance = new web3.eth.Contract(controllerjson, contractAddress);
-
-
-  contractInstance.getPastEvents('allEvents',
-    {fromBlock: 0,  toBlock: 'latest'},
-    (error, logs) => {
-    if (error) console.error(error);
-    logs.forEach(log => {
-        // Do something with log
-        // console.log(log)
-    })
-  })
-
-
-// contractInstance.methods.race_end().call({from:contractAddress},function(error,response){
-//         console.log('end');
-//         console.log('response: ',response);
-//         })
-
-var options={address:contractAddress};
-
-var sub = web3.eth.subscribe('logs',options,function(logs){
-});
-sub.on('data',function(result){
-    console.log(result)
-let contractdetails=(web3.eth.abi.decodeLog([
-            {
-                "indexed": false,
-                "name": "_address",
-                "type": "address"
-            },
-            
-            {
-                "indexed": false,
-                "name": "_owner",
-                "type": "address"
-            },
-            
-            
-            {
-                "indexed": false,
-                "name": "_time",
-                "type": "uint256"
-            }
-        ],result.data,result.topics));
-});
+var db = require('./db');
 
 
 
 
 
-
-
-
-
-// var newevent=contractInstance.methods.race_end()
-
-// newevent.watch(function(error,result){
-//     if(!error)
-//     {
-//         console.log(result);
-//     }
-//     else
-//     {
-//         console.log('Error',error)
-//     }
-// })
-
-
-
-// var contractController = contract(samplejson);
-// // 0x164f38736e38d07e64d2f3a9238db94d8a232a57
-// var contractinstance=web3.eth.contract(samplejson.abi)
-// var contractAddress="0xdb0f8D94aBd0Af1059771ecfD5d831F49BE52bde";
-// var instance=contractinstance.at(contractAddress)
-// instance.race_end.call(function(error,response){
-//         console.log('end');
-//         console.log('response: ',response);
-//         })
-// // console.log(instance)
-// var events=instance.allEvents({fromBlock: 0, toBlock: 'latest'});
-// events.get(function(error, logs){
-//     console.log(logs);
-// });
-// var ContractController = require('./contract/ContractController');
-// app.use('/contract', ContractController);
+var ContractController = require('./contract/ContractController');
+app.use('/contract', ContractController);
 module.exports = app;
