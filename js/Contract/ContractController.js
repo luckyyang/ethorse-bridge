48e8f0072d837eee2511d9fc99758c63d60ff7af
ethorse <snipertech@protonmail.com>
2018-06-14 08:51:45 +0200

    Refactoring active races

--- js/Contract/ContractController.js
+++ js/Contract/ContractController.js
@@ -208,7 +208,8 @@ router.get('/getActiveRaces', function(req, res) {
                 tempjson['active']='Race in progress';
                 if(tempjson.contractid!==undefined)
                 result.push(tempjson);
-            } else if(parseInt(tempjson.date)<=currenttime && (parseInt(tempjson.date)+parseInt(tempjson.betting_duration))>=currenttime){
+            // } else if(parseInt(tempjson.date)<=currenttime && (parseInt(tempjson.date)+parseInt(tempjson.betting_duration))>=currenttime){
+            } else if (parseInt(tempjson.date)+parseInt(tempjson.betting_duration))>=currenttime){
                 tempjson['active']='Open for bets';
                 if(tempjson.contractid!==undefined)
                 result.push(tempjson);
